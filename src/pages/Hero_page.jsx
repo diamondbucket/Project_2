0e90a58ec21ddd19918279ababe9e1c2ca2c1226
D. Pranav <132156340+diamondbucket@users.noreply.github.com>
2024-08-11 10:08:40 +0530

    lol

--- src/pages/Hero_page.jsx
+++ src/pages/Hero_page.jsx
@@ -3,8 +3,12 @@ import logo_2 from "../assets/logo_2.svg";
 import Navbar from "../components/responsive_mini_nav";
 import Circle from "../components/Circle";
 import bottle from "../assets/bottle_with_top_high_res.png";
+<<<<<<< Updated upstream
 import Third_page from "./Third_page";
 import Footer from "./Footer";
+=======
+import SecondPage from "./Second_page"; // Import SecondPage here
+>>>>>>> Stashed changes
 
 const ConcentricCircles = ({ circles }) => {
   return (
@@ -37,6 +41,7 @@ const HeroPage = () => {
   ];
 
   return (
+<<<<<<< Updated upstream
     <div className="relative h-screen w-screen overflow-x-hidden">
       <div className="relative h-screen w-screen bg-white ">
       <div className="relative bg-white w-screen h-20 flex flex-row justify-between items-center px-4 border-b-2 border-gray-300">
@@ -65,17 +70,55 @@ const HeroPage = () => {
               </button>
             </div>
           </div>
+=======
+    <div className="relative w-screen min-h-screen bg-white overflow-x-hidden">
+      <div className="relative h-screen w-screen bg-white">
+        <div className="relative bg-white w-screen h-20 flex flex-row justify-between items-center px-4 border-b-2 border-gray-300">
+          <img src={logo_2} alt="Logo" className="w-20 h-auto" />
+          <Navbar />
+          <button className="bg-black rounded-full w-28 h-10 px-4 text-white flex items-center justify-center">
+            <h1>Order Now</h1>
+          </button>
         </div>
-      )}
-      <div className="w-auto h-auto flex flex-col items-center cursor-pointer">
-        <img
-          src={bottle}
-          className={`absolute h-[800px] ${bottlePosition}  z-10 transition-transform duration-600 ease-out`}
-          onClick={handleBottleClick}
-        />
+        {showText && (
+          <>
+            <div className="w-fit h-fit absolute left-10 text-8xl translate-y-20 font-bold z-20 text-center">
+              <p className="text-transparent bg-clip-text bg-gradient-to-r from-[#222222] via-[#848484] to-white translate-x-3">
+                FIND YOUR QUALITY
+              </p>
+              <div className="translate-x-4 w-fit h-fit">
+                <p className="text-transparent bg-clip-text bg-gradient-to-r from-[#222222] via-[#848484] to-zinc-400">
+                  WATER BOTTLE
+                </p>
+              </div>
+              <div className="w-fit h-fit text-white text-4xl font-normal translate-y-10 translate-x-6">
+                <p>Made exclusive for our champs that are</p>
+                <p>constantly out in the sun working hard</p>
+                <div className="translate-y-full">
+                  <button className="bg-black text-white px-8 py-6 rounded-full border-8 text-4xl border-[#363634]">
+                    ORDER NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+
+        <div className="w-auto h-auto flex flex-col items-center cursor-pointer overflow-hidden">
+          <div className="absolute h-[110vh] top-0 w-4 bg-white z-10 right-64 shadow-[0px_0px_20px_rgba(255,255,255,0.8)]" />
+          <img
+            src={bottle}
+            className={`absolute h-[800px] ${bottlePosition} z-10 transition-transform duration-600 ease-out`}
+            onClick={handleBottleClick}
+          />
+        </div>
+        <div className="absolute -translate-x-5 inset-0 flex items-center top-96 justify-center ">
+          <ConcentricCircles circles={circles} />
+>>>>>>> Stashed changes
+        </div>
       </div>
-      <div className="absolute -translate-x-5 inset-0 flex items-center top-96 justify-center">
-        <ConcentricCircles circles={circles} />
+      <div className="relative z-10">
+        <SecondPage />
       </div>
       </div>
       <Third_page />
---
+++ src/pages/Second_page.jsx
@@ -0,0 +1,5 @@
+const SecondPage = () => {
+  return <div className="relative bg-[#918e93] w-screen h-screen"></div>;
+};
+
+export default SecondPage;
